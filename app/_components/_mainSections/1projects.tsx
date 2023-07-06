1bd9c7f60ab7b8e1f6d0bce67cede4606f403bbc
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2023-07-06 11:28:51 -0500

    Basic Collapsed Proj Container
    Placeholder SVG used. Unreactive. But flows in the document correctly and is styled

--- app/_components/_mainSections/1projects.tsx
+++ app/_components/_mainSections/1projects.tsx
@@ -1,11 +1,28 @@
 export default function Projects() {
     return (
-      <section id="projects" className="scroll-mt-20 flex w-screen sm:pl-16 sm:pt-32 sm:pr-32 sm:font-medium sm:text-4xl">
+      <section id="projects" className="scroll-mt-20 grid grid-cols-2 w-fill pl-2 pr-4 mt-16 mb-4 sm:pl-16 sm:pr-32 sm:font-medium sm:text-4xl">
 
-        <article className="mt-4 mb-4">
-          <h1 className="mb-4 flex-1 font-bold text-4xl text-center xsml:text-5xl">Projects</h1>
-          <p className="text-2xl text-center xsml:text-left xsml:ml-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis tincidunt ornare. Curabitur non nisl molestie, volutpat leo vitae, vulputate est. In hac habitasse platea dictumst. Etiam et arcu vel felis dignissim dapibus. Nunc eu lobortis nibh, a lobortis leo. Quisque nunc arcu, finibus a maximus eget, luctus vel elit. Quisque ornare pellentesque nisi, a mollis mi laoreet in. Fusce rutrum, lorem eget vehicula porttitor, orci sem cursus lacus, ut sagittis nisi nisi aliquet diam. Donec sed est nunc. Sed rhoncus non tortor sit amet cursus.</p>
-        </article>
+          <h1 className="col-span-2 text-virid italic flex-1 font-light text-3xl text-left xsml:text-5xl">Projects</h1>
+          
+          {/* === PASSWORD GENERATOR === */}
+          <div className='col-span-2 flex flex-row justify-between items-center rounded-md w-fill bg-robin'>
+            
+            {/* Wraps SVG Icon */}
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="2.25rem" height="2.25rem" fill="currentColor" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+            </div>
+
+            <h1 className=''>Password Generator</h1>
+
+            <div className='flex flex-col text-center rounded-md text-xs bg-cymru text-mint'>
+              <p>R</p>
+              <p>E</p>
+              <p>P</p>
+              <p>O</p>
+            </div>
+          </div>  
 
       </section>
     )
