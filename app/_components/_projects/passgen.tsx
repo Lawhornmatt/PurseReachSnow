741acec0923dca0f85015d6702c9fa9e15e8c4f1
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2023-07-07 11:31:59 -0500

    Interactable PassGen
    Clicking the positive sign will extend the element and change the plus sign to a minus sign

--- app/_components/_projects/passgen.tsx
+++ app/_components/_projects/passgen.tsx
@@ -3,14 +3,41 @@ import Link from 'next/link';
 import { useState } from "react";
 
 export default function PassGen() {
-    return (
-      <section className='col-span-2 flex flex-row items-center rounded-md bg-robin'>
-            
-        <svg xmlns="http://www.w3.org/2000/svg" width="2.25rem" height="2.25rem" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-        </svg>
+  
+  const [isOpen, setOpen] = useState(false);
+  const openProj = () => { setOpen(!isOpen) };
 
-        <h1 className='text-xl flex-1 text-center'>Password Generator</h1>
-      </section>  
-    )
-  };
+  return (
+    <section className='col-span-2 grid grid-cols-3 items-center rounded-md text-xl bg-robin'>
+          
+      <button onClick={openProj}>
+        {!isOpen && (
+          <svg xmlns="http://www.w3.org/2000/svg" width="2.25rem" height="2.25rem" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+          </svg>
+        )}
+
+        {isOpen && (
+          <svg xmlns="http://www.w3.org/2000/svg" width="2.25rem" height="2.25rem" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+          </svg>
+        )}
+      </button>
+
+      <h1 className='flex-1 text-center'>Password Generator</h1>
+
+      {isOpen && (
+        <div className='row-start-2'>
+            checkbox
+        </div>
+      )}
+
+      {isOpen && (
+        <div className='row-start-3'>
+            checkbox
+        </div>
+      )}
+
+    </section>  
+  )
+};
--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -36,7 +36,7 @@ export default function NavBar() {
                            <Link onClick={toggleMenu} href={url} className="bg-leaf w-full h-16 text-3xl mb-1 mr-1 text-center flex items-center justify-center"><span className="sm:tracking-navbar">{title}</span></Link>
                         ))}
                     </div>
-                )};
+                )}
             </nav>
 
             {/* NAVBAR FOR SCREENS WIDER THAN 360px */}
