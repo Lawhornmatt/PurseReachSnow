72e08ae561caea8d86e4052ab81b7010cbe7465e
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2023-07-26 22:44:03 -0500

    Gist Draft
    Expanded the 'gist of what I do' section. Still subject to revision

--- app/_components/_mainSections/projects.tsx
+++ app/_components/_mainSections/projects.tsx
@@ -27,10 +27,24 @@ export default function Projects() {
         <SkillList />
 
         <div id='bio' className='col-span-2 lg:col-span-3 flex flex-col'>
-          <h2 className='mb-2 text-cymru italic flex-1 font-light text-md text-left xsml:text-xl'>
+          <h2 className='mb-4 text-cymru italic flex-1 font-light text-md text-left xsml:text-xl'>
             A gist of what I do:
           </h2>
-          <p className="text-2xl text-center xsml:text-left xsml:ml-2">yada yada</p>
+          <p className="mb-4 text-2xl text-center indent-6 xsml:text-left xsml:ml-2">
+            I am a junior full-stack developer with a Bachelors in Geology from UT at El Paso. 
+            I've been a project manager / assistant production manager at a plant 
+            and for nearly a decade I was in the service industry. 
+            I take the skills and lessons from those experiences, 
+            the support of the amazing folks I met along the way, 
+            and I build cutting-edge web apps.
+          </p>
+          <p className="text-2xl text-center indent-6 xsml:text-left xsml:ml-2">
+            In 2022, I completed a Full-Stack Web Development certification program by the University of Texas at Austin - CPE. 
+            Since then I have explored more of what makes me interested in web development, e.g. web accessibility, 
+            open source projects, and the independent web. Learning how to create safer, stronger, more equitable 
+            online communities is a goal of mine. I am also looking to utilize tech within the field of Geology, e.g. data-science 
+            for novel data analysis and web development to reach the public with environmental information in engaging ways.
+          </p>
         </div>
 
       </section>
