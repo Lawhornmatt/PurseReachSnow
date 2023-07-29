e22113f81b0d023e1552ebe5ae672772ca50b6c6
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2023-07-29 15:21:48 -0500

    Reduce Mobile Contact
    On smaller screens, i.e. mobile, the contact footer will only be icons instead of icons and labels. I realized I had been struggling making the words fit on smaller screens when there was no reason to show them at all.

--- components/mainSections/contact.tsx
+++ components/mainSections/contact.tsx
@@ -9,35 +9,31 @@ export default function Contact() {
     <section id="contact" className="
         scroll-mt-20 
         text-xl
-        pl-8
-        sm:pl-0
         border-t-2 
         border-cymru 
-        grid 
-        grid-cols-1 
-        sm:grid-cols-2 
-        md:grid-cols-4 
+        flex
+        justify-between
         gap-6 mx-4 mt-8 mb-4 pt-4 
         sm:mx-16 2xl:mx-40">
 
-        <Link className="flex gap-2 justify-start sm:justify-center items-center md:justify-normal" href="https://github.com/Lawhornmatt">
+        <Link className="flex gap-2 justify-center items-center md:justify-normal" href="https://github.com/Lawhornmatt">
           <GitHubIcon />
-          <span className='lg:text-3xl'>Git Hub</span>
+          <span className='hidden md:inline lg:text-3xl'>Git Hub</span>
         </Link>
 
-        <Link className="flex gap-2 justify-start sm:justify-center items-center md:justify-normal" href="https://www.linkedin.com/in/matthew-lawhorn/">
+        <Link className="flex gap-2 justify-center items-center md:justify-normal" href="https://www.linkedin.com/in/matthew-lawhorn/">
           <LinkedInIcon />
-          <span className='lg:text-3xl'>LinkedIn</span>
+          <span className='hidden md:inline lg:text-3xl'>LinkedIn</span>
         </Link>
 
-        <Link className="flex gap-2 justify-start sm:justify-center items-center md:justify-normal" href="https://federate.social/@lawhornmatt">
+        <Link className="flex gap-2 justify-center items-center md:justify-normal" href="https://federate.social/@lawhornmatt">
           <MastodonIcon />
-          <span className='lg:text-3xl'>Mastodon</span>
+          <span className='hidden md:inline lg:text-3xl'>Mastodon</span>
         </Link>
 
-        <Link className="flex gap-2 justify-start sm:justify-center items-center md:justify-normal" href="mailto:lawhornmatt@proton.me">
+        <Link className="flex gap-2 justify-center items-center md:justify-normal" href="mailto:lawhornmatt@proton.me">
           <EmailIcon />
-          <div className='flex flex-col lg:text-2xl'> 
+          <div className='hidden md:flex md:flex-col lg:text-2xl'> 
             <span className='leading-4'>lawhornmatt</span>
             <span className='leading-4'>@proton.me</span>
           </div>
