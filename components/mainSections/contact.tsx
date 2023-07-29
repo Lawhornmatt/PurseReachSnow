8bc9f67364d26d52ac35cf1c7496948b7bc21d3a
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2023-07-29 15:34:12 -0500

    Reduce MediaLinks
    Just like with contacts sections, MediaLinks now are only icons on smaller screens, i.e. mobile. Labels return on medium breakpoint and higher. Also removed justified-center class from MediaLinks and Contact, no longer needed.

--- components/mainSections/contact.tsx
+++ components/mainSections/contact.tsx
@@ -16,22 +16,22 @@ export default function Contact() {
         gap-6 mx-4 mt-8 mb-4 pt-4 
         sm:mx-16 2xl:mx-40">
 
-        <Link className="flex gap-2 justify-center items-center md:justify-normal" href="https://github.com/Lawhornmatt">
+        <Link className="flex gap-2 items-center md:justify-normal" href="https://github.com/Lawhornmatt">
           <GitHubIcon />
           <span className='hidden md:inline lg:text-3xl'>Git Hub</span>
         </Link>
 
-        <Link className="flex gap-2 justify-center items-center md:justify-normal" href="https://www.linkedin.com/in/matthew-lawhorn/">
+        <Link className="flex gap-2 items-center md:justify-normal" href="https://www.linkedin.com/in/matthew-lawhorn/">
           <LinkedInIcon />
           <span className='hidden md:inline lg:text-3xl'>LinkedIn</span>
         </Link>
 
-        <Link className="flex gap-2 justify-center items-center md:justify-normal" href="https://federate.social/@lawhornmatt">
+        <Link className="flex gap-2 items-center md:justify-normal" href="https://federate.social/@lawhornmatt">
           <MastodonIcon />
           <span className='hidden md:inline lg:text-3xl'>Mastodon</span>
         </Link>
 
-        <Link className="flex gap-2 justify-center items-center md:justify-normal" href="mailto:lawhornmatt@proton.me">
+        <Link className="flex gap-2 items-center md:justify-normal" href="mailto:lawhornmatt@proton.me">
           <EmailIcon />
           <div className='hidden md:flex md:flex-col lg:text-2xl'> 
             <span className='leading-4'>lawhornmatt</span>
--- components/pieces/medialinks.tsx
+++ components/pieces/medialinks.tsx
@@ -7,27 +7,35 @@ import EmailIcon from '../icons/email';
 
 export default function SocialMediaLinks() {
     return (
-        <div className= "md:w-1/3 flex flex-col gap-4 md:gap-12 pl-4 md:pl-0 justify-center items-start text-2xl lg:tracking-navbar">
+        <div className="
+            md:w-1/3 
+            flex 
+            md:flex-col 
+            gap-4 md:gap-12 
+            justify-around md:justify-center 
+            items-start 
+            text-2xl 
+            lg:tracking-navbar">
 
-            <Link className="flex gap-6 justify-center md:justify-normal" href="https://github.com/Lawhornmatt">
+            <Link className="flex gap-6" href="https://github.com/Lawhornmatt">
               <GitHubIcon />
-              <span className='md:text-3xl'>Git Hub</span>
+              <span className='hidden md:inline md:text-3xl'>Git Hub</span>
             </Link>
 
-            <Link className="flex gap-6 justify-center md:justify-normal" href="https://www.linkedin.com/in/matthew-lawhorn/">
+            <Link className="flex gap-6" href="https://www.linkedin.com/in/matthew-lawhorn/">
               <LinkedInIcon />
-              <span className='md:text-3xl'>LinkedIn</span>
+              <span className='hidden md:inline md:text-3xl'>LinkedIn</span>
             </Link>
 
-            <Link className="flex gap-6 justify-center md:justify-normal" href="https://federate.social/@lawhornmatt">
+            <Link className="flex gap-6" href="https://federate.social/@lawhornmatt">
               <MastodonIcon />
-              <span className='md:text-3xl'>Mastodon</span>
+              <span className='hidden md:inline md:text-3xl'>Mastodon</span>
             </Link>
 
-            <Link className="flex gap-6 justify-center items-center md:justify-normal" href="mailto:lawhornmatt@proton.me">
+            <Link className="flex gap-6 items-center" href="mailto:lawhornmatt@proton.me">
               <EmailIcon />
               {/* tracking-normal */}
-              <div className='flex flex-col'> 
+              <div className='hidden md:flex md:flex-col'> 
                 <span className='leading-4'>lawhornmatt</span>
                 <span className='leading-4'>@proton.me</span>
               </div>
